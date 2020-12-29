0f3a85298b6ca9254a23838c460d2051656e3afe
yuxillc <54974110+yuxillc@users.noreply.github.com>
2020-12-29 15:15:48 +0800

    v2.0
    student login system done,continue to perfect it

--- app/src/main/java/com/example/student_login_system/Student.java
+++ app/src/main/java/com/example/student_login_system/Student.java
@@ -1,5 +1,7 @@
 package com.example.student_login_system;
 
+import java.util.Objects;
+
 public class Student {
     private String username;
     private String studentID;
@@ -9,37 +11,40 @@ public class Student {
 
     public Student(String username,String email,String password){
         this.username=username;
-        this.studentID="S"+String.format("%06",IDsum);
+        this.studentID="S"+String.format("%06d",IDsum);
         this.email=email;
         this.password=password;
-        IDsum++;
-    }
-    public Student(String username,String password){
-        this.username=username;
-        this.password=password;
-        this.email="";
-        this.studentID="S"+String.format("%06",IDsum);
-        IDsum++;
-    }
-    public Student(String username){
-        this.username=username;
-        this.password="00000000";
-        this.email="";
-        this.studentID="S"+String.format("%06",IDsum);
         IDsum++;
     }
     public Student(){
         this.username="";
         this.password="00000000";
         this.email="";
-        this.studentID="S"+String.format("%06",IDsum);
+        this.studentID="S000001";
         IDsum++;
+    }
+    public Student(String studentID,String password){
+        this.username="";
+        this.password=password;
+        this.email="";
+        this.studentID=studentID;
+    }
+    public Student(String studentID){
+        this.username="";
+        this.password="00000000";
+        this.email="";
+        this.studentID=studentID;
+    }
+    public Student(int ID){
+        this.username="";
+        this.password=null;
+        this.email="";
+        this.studentID="S"+String.format("%06d",ID);
     }
 
     public static int getIDsum() {
         return IDsum;
     }
-
     public static void setIDsum(int IDsum) {
         Student.IDsum = IDsum;
     }
@@ -51,12 +56,8 @@ public class Student {
         this.username = username;
     }
 
-    public String getStudentID() {
-        return studentID;
-    }
-    public void setStudentID(String studentID) {
-        this.studentID = studentID;
-    }
+    public String getStudentID() { return studentID; }
+    public void setStudentID(String studentID) { this.studentID = studentID; }
 
     public String getEmail() {
         return email;
@@ -70,5 +71,17 @@ public class Student {
     }
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public boolean equals(String studentID,String password) {
+        if (this.studentID.equals(studentID) &&
+                this.password.equals(password) )
+            return true;
+        else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, studentID, email, password);
     }
 }
--- app/src/main/java/com/example/student_login_system/StudentLogin.java
+++ app/src/main/java/com/example/student_login_system/StudentLogin.java
@@ -1,14 +1,91 @@
 package com.example.student_login_system;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextClock;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.OnLifecycleEvent;
+
+import java.util.ArrayList;
 
 public class StudentLogin extends AppCompatActivity {
+    ArrayList<Student> StudentList = new ArrayList<Student>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
+
+        StudentList.add(new Student());
+        Toast.makeText(getApplicationContext(), StudentList.get(0).getStudentID(), Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onResume () {
+        super.onResume();
+        TextView tv4;
+        tv4 = (TextView) findViewById(R.id.tv4);
+        String tv4Text = "StudentID,UserName,Password,Email\n";
+
+
+        for (int index = 0,
+             listzise = StudentList.size();
+             index < listzise;
+             index++)
+        {
+            tv4Text += StudentList.get(index).getStudentID() + ",";
+            tv4Text += StudentList.get(index).getUsername() + ",";
+            tv4Text += StudentList.get(index).getPassword() + ",";
+            tv4Text += StudentList.get(index).getEmail() + "\n";
+
+        }
+        tv4.setText(tv4Text);
+    }
+
+//    public void studentLogin(View view) {
+//        int index=0;
+//        String username,studentID,email,password;
+//        EditText StudentID =(EditText) findViewById(R.id.studentID);
+//        EditText Password =(EditText) findViewById(R.id.password);
+//        for(index=0;
+//            index < 1000000 ||
+//                    !new Student(String.valueOf(StudentID.getText()))
+//                            .equals(StudentList.get(index));
+//            index++) ;
+//
+//        if(index>=1000000)
+//            Toast.makeText(getApplicationContext(), "login success", Toast.LENGTH_SHORT).show();
+//        else
+//            Toast.makeText(getApplicationContext(), "login fail", Toast.LENGTH_SHORT).show();
+//    }
+    public void studentLogin(View view) {
+        String username,studentID,email,password;
+        EditText StudentID =(EditText) findViewById(R.id.studentID);
+        EditText Password =(EditText) findViewById(R.id.password);
+        studentID = String.valueOf(StudentID.getText());
+        password = String.valueOf(Password.getText());
+        for (int index = 0,
+             listzise = StudentList.size();
+             index < listzise;
+             index++)
+        {
+            if (StudentList
+                    .get(index)
+                    .equals(studentID,password)) {
+                Toast.makeText(getApplicationContext(),"login success.",Toast.LENGTH_SHORT).show();
+                
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "login fail", Toast.LENGTH_SHORT).show(); }
+
+        }
+
+
+    }
+    public void studentSign(View view) {
     }
 }
