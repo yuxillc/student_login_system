3ef61d34dd2e20a6041b4b44e9ec4c4948e4250f
yuxillc <54974110+yuxillc@users.noreply.github.com>
2021-01-01 13:09:14 +0800

    v2.5
    camera done

--- app/src/main/java/com/example/student_login_system/StudentSign.java
+++ app/src/main/java/com/example/student_login_system/StudentSign.java
@@ -2,12 +2,27 @@ package com.example.student_login_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class StudentSign extends AppCompatActivity {
+    private static final String TAG ="image";
+    static final int REQUEST_IMAGE_CAPTURE = 100;
+    private static Context context;
     String ID="Uffffff";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +46,32 @@ public class StudentSign extends AppCompatActivity {
     }
 
     public void sign(View view) {
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+//        Date date=new Date();
+//        Log.i(TAG,ID+sdf.format(date)+".img");
+//        File img=new File(context.getFilesDir(), ID+sdf.format(date)+".img");
+//        try{
+//            if(img.createNewFile())
+//                System.out.println("文件创建成功！");
+//            else
+//                System.out.println("出错了，该文件已经存在。");
+//        }
+//        catch(IOException ioe) {
+//            ioe.printStackTrace();
+//        }
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
         Toast.makeText(getApplicationContext(),"Sign done.",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap thumbnail = data.getParcelableExtra("data");
+            // Do other work with full size photo saved in locationForPhotos
+            ImageView iv3= (ImageView) findViewById(R.id.imageView3);
+            iv3.setImageBitmap(thumbnail);
+        }
     }
 }
